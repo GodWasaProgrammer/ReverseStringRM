40fee85d2056acd269fcba6b928ebe6ba84d344b
GodWasaProgrammer <bwnils@gmail.com>
2022-12-08 21:10:46 +0100

    Added if statement to check for palondrime in the linq reversal

--- ReversewithLinQ.cs
+++ ReversewithLinQ.cs
@@ -16,11 +16,17 @@ namespace ReverseStringRM
 
             // takes your input
             string input = Console.ReadLine();
+            Console.WriteLine($"You typed: {input}");
 
 
             // creates a string <output> , then makes input into a chararray,reverses,and back to array
             string output = new(input.ToCharArray().Reverse().ToArray());
 
+            if (input == output)
+
+            {
+                Console.WriteLine("This is a Palondrime");
+            }
 
             // types out the reversed output string
 
