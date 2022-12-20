3a7de1af2e1f666a5c56252fd46844ad27699606
GodWasaProgrammer <bwnils@gmail.com>
2022-12-20 02:54:09 +0100

    added newlines.
    Removed some tolower method calls that wasnt necessary.  Less is more. (reversemystring)
    
    Refactored after suggestions from mike regarding ToLower Method calls. (reversewithlinQ)
    
    Much cleaner now.

--- ReverseMyString.cs
+++ ReverseMyString.cs
@@ -2,51 +2,38 @@
 {
     public static class ReverseMyString
     {
-        
         public static void Inputhandler()
         {
-            
             string input;
-            
-
             do
             {
                 Console.WriteLine("Give me a string to reverse!");
-                input = Console.ReadLine();
-                
-
+                input = Console.ReadLine().ToLower();
                 if (input == "")
                 {
                     Console.WriteLine("You have to give me some input!");
                 }
             }
             while (input == "");
-            
 
             char[] CharArray = input.ToCharArray(); // makes a char array out of our string 
             // declare an empty string to fill reversely
             string Reversedstring = string.Empty;
-
             // 
             for (int i = CharArray.Length - 1; i > -1; i--)
             {
                 Reversedstring += CharArray[i];
             }
-
-            string inputtolower = input.ToLower();
-            input = inputtolower;
-
-            string Reversedstringtolower = Reversedstring.ToLower();
-            Reversedstring = Reversedstringtolower;
             // writes out what you put in, before reversal.
             Console.WriteLine($"you typed: " + input);
+            Console.WriteLine($"what you typed reversed: " + Reversedstring);
             // returns the string who has been reversed
             // if input is the same as reversed string and input is not equal to ""
             if (input == Reversedstring)
             {
                 Console.WriteLine("This is a Palindrome");
             }
-            
+
         }
     }
 }
--- ReversewithLinQ.cs
+++ ReversewithLinQ.cs
@@ -11,23 +11,22 @@
             {
                 Console.WriteLine("input your string to reverse by LinQ!");
                 input = Console.ReadLine();
+                input= input.ToLower();
                 if (input == "")
                 {
                     Console.WriteLine("Give me input! you shall not pass otherwise!");
                 }
+
             } while (input == "");
 
             Console.WriteLine($"You typed: {input}");
             // creates a string <output> , then makes input into a chararray,reverses,and back to array
             string output = new(input.ToCharArray().Reverse().ToArray());
-            string outputtolower = output.ToLower();
-            output = outputtolower;
-            string inputtolower = input.ToLower();
-            input = inputtolower;
             if (input == output)
             {
                 Console.WriteLine("This is a Palindrome");
             }
+
             // types out the reversed output string
             Console.WriteLine(output);
         }
