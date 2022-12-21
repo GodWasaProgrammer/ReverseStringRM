a638cb176c0896233fa5a5376b6602db0f451658
GodWasaProgrammer <bwnils@gmail.com>
2022-12-21 01:45:45 +0100

    Refactored the entire thing, removed the 2 basically empty classes who had 1 purpose each, put it all in main, used an array accessor operator in for loop instead

--- Program.cs
+++ Program.cs
@@ -4,11 +4,45 @@
     {
         static void Main()
         {
-            ReverseMyString.Inputhandler();
-             // cw. my class, method inputhandler, your input
-            ReversewithLinQ.ReversebyLinQ(); // class,methodcall
+            string input;
+            do
+            {
+                Console.WriteLine("Give me a string to reverse!");
+                input = Console.ReadLine().ToLower();
+                if (input == "")
+                {
+                    Console.WriteLine("You have to give me some input!");
+                }
+            }
+            while (input == "");
+            
+            string Reversedstring = string.Empty;
+            // 
+            for (int i = input.Length - 1; i > -1; i--)
+            {
+                Reversedstring += input[i];
+            }
 
-            Console.ReadKey();
+            // writes out what you put in, before reversal.
+            Console.WriteLine($"you typed: " + input);
+            Console.WriteLine($"what you typed reversed: " + Reversedstring);
+            // returns the string who has been reversed
+            
+            if (input == Reversedstring)
+            {
+                Console.WriteLine("This is a Palindrome");
+            }
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("This also reverses, but this one by Linq");
+            Console.WriteLine($"You typed: {input}");
+            // creates a string <output> , then makes input into a chararray,reverses,and back to array
+            string output = new(input.ToCharArray().Reverse().ToArray());
+            if (input == output)
+            {
+                Console.WriteLine("This is a Palindrome");
+            }
+            // types out the reversed output string
+            Console.WriteLine($"What you typed but reversed:{output}");
         }
     }
 }
